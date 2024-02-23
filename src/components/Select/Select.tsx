3e15d8f58ec0ce39a25dc3cfe0e85d3acdcab284
nosfair <nosfairalchtion@gmail.com>
2024-02-23 17:17:57 +0100

    add Select and function for search

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent } from 'react';
+
+/**
+ * Select - Component - Select number for rendering list per page
+ * 
+ * @component
+ * @param {Function} props.setCurrentPage - Function to set the current page.
+ * @param {Function} props.setElementsPerPage - Function to set the number of element per pages.
+ * @param {string} props.colorPrimary - Primary color for styling.
+ * @returns {JSX.Element} - Pagination component JSX element.
+ * 
+ * @author Nosfairal
+ * 
+ */
+
+interface SelectProps {
+    setElementsPerPage: (elements: number) => void;
+    setCurrentPage: (page: number) => void;
+    colorPrimary: string;
+}
+
+const Select = ({ setElementsPerPage, setCurrentPage, colorPrimary }: SelectProps) => {
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setCurrentPage(1);
+        setElementsPerPage(parseInt(e.target.value));
+    };
+
+    return (
+        <>
+            <select
+                aria-label='select the number of elements to display'
+                onChange={handleChange}
+                style={{
+                    background: colorPrimary,
+                    color: "white",
+                    border: "none",
+                    borderRadius: "5px",
+                    height: "21px",
+                    margin: "none"
+                }}
+            >
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+        </>
+    );
+};
+
+export default Select;
---
+++ src/components/utils/function.ts
@@ -0,0 +1,31 @@
+/**
+ * Function Search in an object's with attributes array
+ * 
+ * @param { string } word - Word to search for  
+ * @param { Object } item - Object to search within
+ * @param { string[] } attributes - attributes to search for the word
+ * @returns {string[] | null} - array of matched values or null if no match
+ * 
+ * @author Nosfairal
+ * 
+ */
+export const search = (word: string, item: any, attributes: string[]): string[] | null => {
+    const match = attributes.some((attribute) => {
+        if (
+            item[attribute] &&
+            item[attribute].toLowerCase().includes(word.toLowerCase())
+        ) {
+            return true;
+        }
+        return false;
+    });
+
+    if (match) {
+        const result = attributes
+            .map((attribute) => item[attribute] || "")
+            .slice(0, 2);
+        return result;
+    }
+
+    return null;
+};
